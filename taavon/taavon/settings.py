9e90c273567c3ea833301cb3b3d5396ebd52ea0f
حمید لقا <96140332@Razaviedu.ir>
2022-11-01 08:51:53 +0330

    Update settings.py

--- taavon/taavon/settings.py
+++ taavon/taavon/settings.py
@@ -111,13 +111,23 @@ WSGI_APPLICATION = 'taavon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fh',
+        'USER': 'postgres',
+        'PASSWORD': 'P@s$w0rd123!@#',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
